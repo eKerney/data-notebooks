f50c941ad5b553b0376a292ee13073d897e13a03
ekerney <kerneyneels@gmail.com>
2025-02-26 23:13:00 -0500

    west main traffic map

---
+++ kzoo_streetlight/colormaps.py
@@ -0,0 +1,12 @@
+seq = ['Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds', 'YlOrBr',
+       'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu',
+       'PuBuGn', 'BuGn', 'YlGn']
+seq2 = ['binary', 'gist_yarg', 'gist_gray', 'gray', 'bone', 'pink', 'spring',
+        'summer', 'autumn', 'winter', 'cool','Wistia', 'hot', 'afmhot',
+        'gist_heat', 'copper']
+uni = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'cool', 'Wistia', 'PiYG']
+unir = ['viridis_r', 'plasma_r', 'inferno_r', 'magma_r', 'cividis_r', 'cool_r', 'Wistia_r']
+div = ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu', 'RdYlGn',
+       'Spectral', 'coolwarm', 'bwr', 'seismic']
+divr = ['PiYG_r', 'PRGn_r', 'BrBG_r', 'PuOr_r', 'RdGy_r', 'RdBu_r', 'RdYlBu_r',
+        'RdYlGn_r', 'Spectral_r', 'coolwarm_r', 'bwr_r', 'seismic_r']
